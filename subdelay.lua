7049bdf79adc2a3d25e68a359dad88bde5698186
Itay Perl <itay.perl@gmail.com>
2018-10-04 00:38:59 +0300

    subdelay.lua: apply the CLI --sub-delay value to external subtitles

--- subdelay.lua
+++ subdelay.lua
@@ -1,5 +1,7 @@
 delays = {}
 
+init_delay = mp.get_property_number('sub-delay')
+
 function on_delay_changed(name, delay)
     sid = mp.get_property_number('sid')
     if sid == nil then
@@ -15,6 +17,15 @@ function on_sid_changed(name, sid)
     end
     if delays[sid] == nil then
         delays[sid] = 0
+        num_tracks = mp.get_property_number("track-list/count")
+        for i = 1, num_tracks do
+            if mp.get_property(string.format("track-list/%d/type", i)) == "sub" and
+               mp.get_property_number(string.format("track-list/%d/id", i)) == sid and
+               mp.get_property_bool(string.format("track-list/%d/external", i)) then
+
+               delays[sid] = init_delay
+            end
+        end
     end
     mp.set_property_number('sub-delay', delays[sid])
 end
